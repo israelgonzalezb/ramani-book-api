49c3c8f8a230ba2633a80ec1078aae48361c254a
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2021-12-20 08:25:06 -0800

    chore: initial commit with book schema

---
+++ src/schemas/book.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+
+import { Review } from '../review/review.schema';
+
+@Schema()
+export class Book {
+  _id: MongooseSchema.Types.ObjectId;
+  @Prop()
+  bookName: string;
+  @Prop()
+  author: string;
+  @Prop()
+  releaseDate: MongooseSchema.Types.Date;
+  @Prop()
+  genre: string;
+  @Prop({ type: [{ type: MongooseSchema.Types.ObjectId, ref: 'Review' }] })
+  reviews: Review[]; // Array of Review Schema
+}
+
+export type BookDocument = Book & Document;
+
+export const BookSchema = SchemaFactory.createForClass(Book);
